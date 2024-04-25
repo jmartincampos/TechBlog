0bb15a98c249ee87034a1eaca28a68d77c1637f5
jmartincampos <jmartincampos18@gmail.com>
2024-04-24 22:36:56 -0700

    changes

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-const User = require('./user');
-const Blog = require('./blog');
-const Comment = require('./comment');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Other imports and middleware
 
-User.hasMany(Comment, { foreignKey: 'user_id' });
-Comment.belongsTo(User, { foreignKey: 'user_id' });
+// Routes
+app.use('/', require('./controllers/homeController'));
+app.use('/auth', require('./controllers/authController'));
+app.use('/post', require('./controllers/postController'));
 
-User.hasMany(Blog, { foreignKey: 'user_id' });
-Blog.belongsTo(User, { foreignKey: 'user_id' });
-
-Blog.hasMany(Comment, { foreignKey: 'blog_id' });
-Comment.belongsTo(Blog, { foreignKey: 'blog_id' });
-
-
-
-module.exports = {
-  User,
-  Blog,
-  Comment,
-}
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
